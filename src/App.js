b89f4490644a7e16840cb2fc493e01c92cd2a818
vanshjain2810 <vanshvasu2810@gmail.com>
2020-08-24 23:37:43 +0530

    monster's name insert

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monster : []
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monster:users}))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monster.map(mon =>
+          <h1 key={mon.id}>{mon.name}</h1>)
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
