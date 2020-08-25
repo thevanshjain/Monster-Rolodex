52800aa41573f4784c4c9dc0484e823a278a06a6
vanshjain2810 <vanshvasu2810@gmail.com>
2020-08-25 19:07:51 +0530

    Updates

---
+++ precache-manifest.692ea14a66d6ad0122dc305879079314.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80edeca7170ddcfb4b175ddf6b97d269",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "57a4f57479eb237a15bd",
+    "url": "/Monster-Rolodex/static/css/main.a1af469e.chunk.css"
+  },
+  {
+    "revision": "9dadd1a5607abc36395e",
+    "url": "/Monster-Rolodex/static/js/2.db3844ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.db3844ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57a4f57479eb237a15bd",
+    "url": "/Monster-Rolodex/static/js/main.760841f7.chunk.js"
+  },
+  {
+    "revision": "709b6bde648e68a1eaa9",
+    "url": "/Monster-Rolodex/static/js/runtime-main.594c4491.js"
+  }
+]);
